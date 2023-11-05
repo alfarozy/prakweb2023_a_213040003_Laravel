c843d01caa62e7ad52f4a4ad78af65397d0f3365
alfarozy.an <mr.alfarozy.a.n@gmail.com>
2023-11-05 11:19:04 +0700

    V11. N+1 Problem & bugfix: route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     {
         return view('posts.index', [
             'title' => "Posts",
-            'posts' => Post::latest()->get()
+            "posts" => Post::with(['author', 'category'])->latest()->get()
         ]);
     }
 
@@ -19,7 +19,7 @@ class PostController extends Controller
     {
         return view('posts.show', [
             'title' => "Single Post",
-            'post' => $post
+            'post' => $post->load(['author', 'category'])
         ]);
     }
 }
